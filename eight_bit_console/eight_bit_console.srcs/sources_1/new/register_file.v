3514927324af5f6c0cb155cfa57ef30f7b099b3d
Carson Bush <hyperdriveguy@gmail.com>
2024-03-27 11:16:37 -0600

    Add register file

---
+++ eight_bit_console/eight_bit_console.srcs/sources_1/new/register_file.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/27/2024 11:14:25 AM
+// Design Name: 
+// Module Name: register_file
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module register_file(
+    input write_enable
+    );
+endmodule
