15a06c5a78b2ea98a14259371a5df30a1ef41e02
Carson Bush <hyperdriveguy@gmail.com>
2024-03-30 14:46:10 -0600

    Add wide mode to register file, add testbench Need to add logic for pairing register f and 0

--- eight_bit_console/eight_bit_console.srcs/sources_1/new/register_file.v
+++ eight_bit_console/eight_bit_console.srcs/sources_1/new/register_file.v
@@ -22,13 +22,17 @@
 
 module register_file(
     input clk,
+    input wide_mode,
     input we,
     input [7:0] data,
+    input [7:0] data_wide,
     input [3:0] dest_sel,
     input [3:0] src_a_sel,
     input [3:0] src_b_sel,
     output [7:0] port_a,
     output [7:0] port_b,
+    output [7:0] port_a_wide,
+    output [7:0] port_b_wide,
     input we_flags,
     input [7:0] new_flags,
     output reg [7:0] flags
@@ -41,6 +45,7 @@ module register_file(
     always @(posedge clk) begin
         if (we) begin
             registers[dest_sel] <= data;
+            if (wide_mode) registers[dest_sel + 1] <= data_wide;
         end
         if (we_flags) begin
             flags <= new_flags;
@@ -49,5 +54,7 @@ module register_file(
     
     assign port_a = registers[src_a_sel];
     assign port_b = registers[src_b_sel];
+    assign port_a_wide = registers[src_a_sel + 1];
+    assign port_b_wide = registers[src_b_sel + 1];
     
 endmodule
